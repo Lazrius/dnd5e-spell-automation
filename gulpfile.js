299393a26cbbf746bac77914985c027fb3539bb7
Lazrius <lazdisco@gmail.com>
2022-09-23 18:45:01 +0100

    v0.0.2

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 import { packFile } from './gulps/pack.js';
 import { generateIds } from './gulps/generateIds.js';
 import { aa } from './gulps/aa.js';
+import { update } from './gulps/update.js';
 
 export const packDbg = () => {
 	process.env.DEBUG = true;
@@ -10,3 +11,4 @@ export const packDbg = () => {
 export const pack = packFile;
 export const ids = generateIds;
 export const autorec = aa;
+export const publish = update;
